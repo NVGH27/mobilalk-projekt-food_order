2501c73919907e7a8167fb267b96eb787c8bdeea
Végh Noémi <h379600@stud.u-szeged.hu>
2025-04-13 23:48:15 +0200

    első mérföldkő befejezése

--- app/src/main/java/com/example/foodorderapp/CartActivity.java
+++ app/src/main/java/com/example/foodorderapp/CartActivity.java
@@ -1,49 +1,23 @@
 package com.example.foodorderapp;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.Firebase;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
-public class RestaurantListActivity extends AppCompatActivity {
-    private static final String LOG_TAG = RestaurantListActivity.class.getName();
+public class CartActivity extends AppCompatActivity {
 
-    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        setContentView(R.layout.activity_restaurant_list);
-
-        TextView title = findViewById(R.id.favoriteTitle);
-        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        title.startAnimation(fadeIn);
-
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser != null) {
-            Log.d(LOG_TAG, "User is logged in: " + currentUser.getEmail());
-        }else{
-            Log.d(LOG_TAG, "No user is logged in");
-            finish();
-        }
+        setContentView(R.layout.activity_cart);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
-        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        bottomNavigationView.setSelectedItemId(R.id.nav_cart);
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int itemId = item.getItemId();
             if (itemId == R.id.nav_home) {
